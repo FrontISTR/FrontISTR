6af72b2b22409b9b61e7a8a5af944367fc1fd949
suemitsu <suemitsu@575e276f-71f8-4965-9d1f-d4eeffdffdfb>
2011-12-09 01:35:49 +0000

    update by Kitayama
    git-svn-id: http://istr.iis.u-tokyo.ac.jp/svn/istr/trunk@34 575e276f-71f8-4965-9d1f-d4eeffdffdfb

--- hecmw1/src/solver/solver_direct_parallel/matrix_repart.c
+++ hecmw1/src/solver/solver_direct_parallel/matrix_repart.c
@@ -3,9 +3,12 @@
  *  Software Name : HPC-MW Library for PC-cluster                      *
  *        Version : 1.00                                               *
  *                                                                     *
- *    Last Update : 2004/06/04                                         *
+ *    Last Update : 2010/06/16                                         *
+ *    Modified by : KITAYAMA Takeshi (Univ. of Tokyo)                  *
+ *                                                                     *
  *       Category : Dynamic Load Balancing                             *
  *         Author : Li Chen (RIST)                                     *
+ *                                                                     *
  *                                                                     *
  *                The University of Tokyo, FSIS Project                *
  *                                                                     *
@@ -63,7 +66,17 @@ void bi_part_directive(int *neqns, int *nttbr, int *irow, int *jcol, int *num_gr
   iperm=(int *)calloc(num_of_col, sizeof(int));
   if((perm==NULL) || (iperm==NULL))
 	  separator_memory_exit("matrix_repart: perm, iperm");
-	options[0]=0;
+
+  /* followings are options. see METIS manual for METIS_NODEND() */
+	options[0]=1; /* specify parameters */
+	options[1]=3; /* default */
+	options[2]=1; /* default */
+	options[3]=2; /* default */
+	options[4]=0; /* default */
+	options[5]=0; /* do not try to compress the matrix */
+	options[6]=0; /* default */
+	options[7]=1; /* default */
+
   num_flag=0;
   METIS_NodeND(&graph->nvtxs, graph->xadj, graph->adjncy, &num_flag, options, perm, iperm);
   /* copy to separator */
