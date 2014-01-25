2b6451419e1489c229ea206fcae8b80dc32a7e1e
Kazuya Goto <goto@pexprocs.jp>
2014-01-25 20:21:46 +0900

    Enabled build without Metis

--- hecmw1/src/solver/solver_direct_parallel/matrix_repart.c
+++ hecmw1/src/solver/solver_direct_parallel/matrix_repart.c
@@ -18,28 +18,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "hecmw_util.h"
+
+#ifdef HECMW_WITH_METIS
 #include "metis.h"
+#endif
+
 #include "matrix2graph.h"
 #include "separator.h"
 Separator_result *separator;
 
 void bi_part_directive(int *neqns, int *nttbr, int *irow, int *jcol, int *num_graph1, int *num_graph2, int *num_separator)
 {
+#ifdef HECMW_WITH_METIS
   graph_type *graph;
   int num_of_row, num_of_col, num_of_nzero;
 
   int *perm, *iperm;
-  
-  
-	num_of_row=*neqns;
-	num_of_col=*neqns;
-	num_of_nzero=*nttbr;
-   
-  
+
+
+  num_of_row=*neqns;
+  num_of_col=*neqns;
+  num_of_nzero=*nttbr;
+
+
   graph=(graph_type *)malloc(sizeof(graph_type));
   if(graph==NULL) 
      separator_memory_exit("graph");
-  
+
   fprintf(stderr, "Start transforming matrix to graph\n");
   matrix2graph(num_of_row, num_of_col, num_of_nzero, irow, jcol, graph);
   fprintf(stderr, "Graph Information ---------------------------------------------------\n");
@@ -47,7 +53,7 @@ void bi_part_directive(int *neqns, int *nttbr, int *irow, int *jcol, int *num_gr
   perm=(int *)calloc(num_of_col, sizeof(int));
   iperm=(int *)calloc(num_of_col, sizeof(int));
   if((perm==NULL) || (iperm==NULL))
-	  separator_memory_exit("matrix_repart: perm, iperm");
+    separator_memory_exit("matrix_repart: perm, iperm");
 
 #if defined(METIS_VER_MAJOR) && (METIS_VER_MAJOR == 5)
   {
@@ -75,10 +81,15 @@ void bi_part_directive(int *neqns, int *nttbr, int *irow, int *jcol, int *num_gr
   }
 #endif
   /* copy to separator */
-  
-	*num_graph1=separator->num_of_lgraph;
-	*num_graph2=separator->num_of_rgraph;
-	*num_separator=separator->num_of_separator;
+
+  *num_graph1=separator->num_of_lgraph;
+  *num_graph2=separator->num_of_rgraph;
+  *num_separator=separator->num_of_separator;
+
+#else
+  fprintf(stderr, "Error: Direct Parallel Solver not available. Please install Metis.\n");
+  HECMW_abort(HECMW_comm_get_comm());
+#endif
 
   return;
 }
