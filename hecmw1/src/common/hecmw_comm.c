6c7dc4e74bc9cbb00cf3961a611efa5c5ff2535a
inagaki <inagaki.kazuhisa@gmail.com>
2015-09-06 15:32:13 +0900

    modified hecmw_AllReduce in serial mode

--- hecmw1/src/common/hecmw_comm.c
+++ hecmw1/src/common/hecmw_comm.c
@@ -411,7 +411,21 @@ HECMW_Allreduce( void *sendbuf, void *recvbuf, int count,
 error:
     return -1;
 #else
-	return 0;
+	if( datatype == HECMW_INT ) {
+        memcpy(recvbuf, sendbuf, sizeof(int)*count);
+	} else if( datatype == HECMW_DOUBLE ) {
+        memcpy(recvbuf, sendbuf, sizeof(double)*count);
+	} else if( datatype == HECMW_CHAR ) {
+        memcpy(recvbuf, sendbuf, sizeof(char)*count);
+	} else {
+        HECMW_set_error( HECMW_ALL_E1003, "Invalid data type is found" );
+        goto error;
+    }
+
+    return 0;
+
+error:
+    return -1;
 #endif
 }
 
