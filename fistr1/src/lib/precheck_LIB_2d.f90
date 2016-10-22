0e7d7f921fcc948d5c50ad3fdbc8537e3db7a00c
morita <morita@multi.k.u-tokyo.ac.jp>
2016-10-22 12:35:21 +0900

    Changed header in lib

--- fistr1/src/lib/precheck_LIB_2d.f90
+++ fistr1/src/lib/precheck_LIB_2d.f90
@@ -1,16 +1,7 @@
-!======================================================================!
-!                                                                      !
-! Software Name : FrontISTR Ver. 3.7                                   !
-!                                                                      !
-!      Module Name : lib                                               !
-!                                                                      !
-!            Written by Yasuji Fukahori (Univ. of Tokyo)               !
-!                                                                      !
-!      Contact address :  IIS,The University of Tokyo, CISS            !
-!                                                                      !
-!      "Structural Analysis for Large Scale Assembly"                  !
-!                                                                      !
-!======================================================================!
+!-------------------------------------------------------------------------------
+! Copyright (c) 2016 The University of Tokyo
+! This software is released under the MIT License, see LICENSE.txt
+!-------------------------------------------------------------------------------
 !> This module provides function to check input data of 2d static analysis
 module m_precheck_LIB_2d
    contains
