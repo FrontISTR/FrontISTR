0e2d5e2482e501e4a37515ef7d3f454eac75c369
Jun Yin <yin@astom.co.jp>
2017-06-21 04:26:17 +0000

    Update fstr_ctrl_material.f90 line 276 should be deleted. It results in error in the following "call init_table" (line 302) when dependencies = 1.

--- fistr1/src/common/fstr_ctrl_material.f90
+++ fistr1/src/common/fstr_ctrl_material.f90
@@ -273,7 +273,7 @@ integer function fstr_ctrl_get_VISCOELASTICITY( ctrl, mattype, nlgeom, dict )
         depends = 0
         rcode = fstr_ctrl_get_param_ex( ctrl, 'DEPENDENCIES  ', '# ',           0,   'I',   depends )
         if( depends>1 ) depends=1   ! temperature depends only currently
-        depends = 0
+        !depends = 0
         nlgeom = TOTALLAG   !default value
         if( fstr_ctrl_get_param_ex( ctrl, 'INFINITE ',  '# ',    0,   'E',   ipt )/= 0) return
         if( ipt/=0 ) nlgeom = INFINITE
