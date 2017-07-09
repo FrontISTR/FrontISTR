765f7e2da504c41e3727fb004dda227ab9e2a611
morita <morita@multi.k.u-tokyo.ac.jp>
2017-07-09 15:56:31 +0900

    Fixed main.c

--- fistr1/src/main/main.c
+++ fistr1/src/main/main.c
@@ -9,91 +9,86 @@ void help(){
   printf("-h: Show this help message.\n");
   printf("-c <Path of control file>: Use this control file. Default ./hecmw_ctrl.dat\n");
   printf("-v: Show version.\n");
-	exit(0);
+  exit(0);
 }
 
 int main( int argc, char *argv[] ){
   int i;
-  for(i = 0; i < argc; ++i){ 
-	  if(*argv[i] == '-') {
-	    switch(*(argv[i]+1)){
-        case 'h': 
-        case 'H': 
-        	help();
-        	break;
+  for(i = 0; i < argc; ++i){
+    if(*argv[i] == '-') {
+      switch(*(argv[i]+1)){
+        case 'h':
+        case 'H':
+          help();
+          break;
         case 'v':
         case 'V':
-        	version();
-        	break;
+          version();
+          break;
         case 'c':
-				  i++;
-				  printf("Sorry this option cannot work yet. (%s)\n",argv[i]);
-				  exit(0);
-        	break;
-	    }	  	
-	  }
+          i++;
+          printf("Sorry this option cannot work yet. (%s)\n",argv[i]);
+          exit(0);
+          break;
+      }
+    }
   }
   fstr_main();
   return 0;
 }
 
-
 void version(){
-  
-  printf("FrontISTR version %d.%d.%d (%s) \n\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, GIT_HASH);
+  printf("FrontISTR version %d.%d.%d (%s) \n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, GIT_HASH);
+  #ifdef WITH_MPI
+    printf("MPI: Enabled\n");
+  #else
+    printf("MPI: Disabled\n");
+  #endif
   #ifndef OPENMP_UNKNOWN
     #ifdef WITH_OPENMP
-    printf("OpenMP\tEnabled\n");
+    printf("OpenMP: Enabled\n");
     #else
-    printf("OpenMP\tDisabled\n");
+    printf("OpenMP: Disabled\n");
     #endif
   #else
-    printf("OpenMP\tUnknown\n");
-  #endif
-  #ifdef WITH_MPI
-  printf("MPI\tEnabled\n");
-  #else
-  printf("MPI\tDisabled\n");
+    printf("OpenMP: Unknown\n");
   #endif
   #ifdef HECMW_METIS_VER
-  printf("HECMW_METIS_VER=%d\n",HECMW_METIS_VER);
+    printf("HECMW_METIS_VER: %d\n",HECMW_METIS_VER);
   #endif
-  printf("\n");
-  printf("Compile Option: \n");
+
+  printf("Compile Option: ");
   #ifdef WITH_MPI
-  printf("-p ");
+    printf("-p ");
   #endif
   #ifdef WITH_TOOLS
-  printf("--with-tools ");
+    printf("--with-tools ");
   #endif
   #ifdef WITH_REFINER
-  printf("--with-refiner ");
+    printf("--with-refiner ");
   #endif
   #ifdef WITH_METIS
-  printf("--with-metis ");
+    printf("--with-metis ");
   #endif
   #ifdef WITH_MUMPS
-  printf("--with-mumps ");
+    printf("--with-mumps ");
   #endif
   #ifdef WITH_LAPACK
-  printf("--with-lapack ");
+    printf("--with-lapack ");
   #endif
   #ifdef WITH_ML
-  printf("--with-ml ");
+    printf("--with-ml ");
   #endif
   #ifdef WITH_PARMETIS
-  printf("--with-parmetis ");
+    printf("--with-parmetis ");
   #endif
   #ifdef WITH_MKL
-  printf("--with-mkl ");
+    printf("--with-mkl ");
   #endif
   #ifdef WITH_PARACON
-  printf("--with-paracon ");
+    printf("--with-paracon ");
   #endif
   printf("\n");
 
-
-  printf("\n");
-  
   exit(0);
 }
