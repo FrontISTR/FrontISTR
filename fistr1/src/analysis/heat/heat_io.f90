a32c28ea3df4f283a8c666c4a0c7380f7293bcba
morita <morita@multi.k.u-tokyo.ac.jp>
2018-10-10 14:19:43 +0900

    Added heat_solve_main.f90

---
+++ fistr1/src/analysis/heat/heat_io.f90
@@ -0,0 +1,155 @@
+!-------------------------------------------------------------------------------
+! Copyright (c) 2016 The University of Tokyo
+! This software is released under the MIT License, see LICENSE.txt
+!-------------------------------------------------------------------------------
+!> This module provides a function to control heat analysis
+module m_heat_io
+
+contains
+
+  subroutine heat_input_restart(fstrHEAT, hecMESH, tstep, tt)
+    use m_fstr
+    implicit none
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    integer(kind=kint) :: restart_step(1)
+    real(kind=kreal)   :: restart_time(1)
+    integer(kind=kint) :: i, tstep
+    real(kind=kreal)   :: tt
+
+    if(fstrHEAT%restart_nout < 0)then
+      fstrHEAT%restart_nout = -fstrHEAT%restart_nout
+      call hecmw_restart_open()
+      call hecmw_restart_read_int(restart_step)
+      call hecmw_restart_read_real(restart_time)
+      call hecmw_restart_read_real(fstrHEAT%TEMP0)
+      call hecmw_restart_close()
+      tstep = restart_step(1)
+      tt = restart_time(1)
+
+      do i = 1, hecMESH%n_node
+        fstrHEAT%TEMPC(i)= fstrHEAT%TEMP0(i)
+        fstrHEAT%TEMP (i)= fstrHEAT%TEMP0(i)
+      enddo
+      write(ILOG,*) ' Restart read of temperatures: OK'
+    endif
+  end subroutine heat_input_restart
+
+  subroutine heat_output_log(hecMESH, fstrPARAM, fstrHEAT, tstep, ctime)
+    use m_fstr
+    implicit none
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    type(fstr_param)          :: fstrPARAM
+    integer(kind=kint) :: i, in, inod, tstep, nmax, nmin
+    real(kind=kreal)   :: temp, ctime, tmax, tmin
+
+    tmax = -1.0d10
+    tmin =  1.0d10
+    nmax = -1
+    nmin = -1
+
+    write(ILOG,*)
+    write(ILOG,'(a,i6)')    ' ISTEP =', tstep
+    write(ILOG,'(a,f10.3)') ' Time  =', ctime
+
+    do i = 1, hecMESH%nn_internal
+      inod = fstrPARAM%global_local_id(1,i)
+      in = fstrPARAM%global_local_id(2,i)
+      temp = fstrHEAT%TEMP(in)
+      if(tmax < temp)then
+        tmax = temp
+        nmax = inod
+      endif
+      if(temp < tmin)then
+        tmin = temp
+        nmin = inod
+      endif
+    enddo
+
+    write(ILOG,'(a,f10.3,i10)') ' Maximum Temperature :', tmax
+    write(ILOG,'(a,i10)')       ' Maximum Node No.    :', nmax
+    write(ILOG,'(a,f10.3,i10)') ' Minimum Temperature :', tmin
+    write(ILOG,'(a,i10)')       ' Minimum Node No.    :', nmin
+  end subroutine heat_output_log
+
+  subroutine heat_output_result(hecMESH, fstrHEAT, tstep, max_step)
+    use m_fstr
+    implicit none
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    integer(kind=kint) :: restart_step(1)
+    real(kind=kreal)   :: restart_time(1)
+    integer(kind=kint) :: i, tstep, max_step
+    character(len=HECMW_HEADER_LEN) :: header
+    character(len=HECMW_NAME_LEN)   :: label
+    character(len=HECMW_NAME_LEN)   :: nameID
+
+    if(IRESULT == 1 .and. (mod(tstep, IRRES) == 0 .or. tstep == max_step))then
+      header = '*fstrresult'
+      call hecmw_result_init(hecMESH, max_step, tstep, header)
+      label = 'TEMPERATURE'
+      call hecmw_result_add(1, 1, label, fstrHEAT%TEMP)
+      nameID = 'fstrRES'
+      call hecmw_result_write_by_name(nameID)
+      call hecmw_result_finalize
+    endif
+  end subroutine heat_output_result
+
+  subroutine heat_output_visual(hecMESH, fstrRESULT, fstrHEAT, tstep, max_step)
+    use m_fstr
+    use m_hecmw2fstr_mesh_conv
+    implicit none
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    type(hecmwST_result_data) :: fstrRESULT
+    integer(kind=kint) :: i, tstep, max_step, interval
+
+    if(IVISUAL == 1 .and. (mod(tstep, IWRES) == 0 .or. tstep == max_step))then
+      interval = IWRES
+      call hecmw_nullify_result_data(fstrRESULT)
+      fstrRESULT%nn_component = 1
+      fstrRESULT%ne_component = 0
+      allocate(fstrRESULT%nn_dof(1))
+      allocate(fstrRESULT%node_label(1))
+      allocate(fstrRESULT%node_val_item(hecMESH%n_node))
+      fstrRESULT%nn_dof(1) = 1
+      fstrRESULT%node_label(1) = 'TEMPERATURE'
+      fstrRESULT%node_val_item = fstrHEAT%TEMP
+      call fstr2hecmw_mesh_conv(hecMESH)
+      call hecmw_visualize_init
+      call hecmw_visualize (hecMESH, fstrRESULT, tstep, max_step, interval)
+      call hecmw_visualize_finalize
+      call hecmw2fstr_mesh_conv(hecMESH)
+      call hecmw_result_free(fstrRESULT)
+    endif
+  end subroutine heat_output_visual
+
+  subroutine heat_output_restart(hecMESH, fstrHEAT, tstep, max_step, current_time)
+    use m_fstr
+    implicit none
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    integer(kind=kint) :: restart_step(1)
+    real(kind=kreal)   :: restart_time(1)
+    integer(kind=kint) :: restrt_data_size
+    integer(kind=kint) :: tstep, max_step
+    real(kind=kreal)   :: current_time
+
+    if(0 < fstrHEAT%restart_nout .and. (mod(tstep, fstrHEAT%restart_nout) == 0 .or. tstep == max_step))then
+      restart_step(1) = tstep
+      restart_time(1) = current_time
+      restrt_data_size = size(restart_step)
+      call hecmw_restart_add_int(restart_step, restrt_data_size)
+      restrt_data_size = size(restart_time)
+      call hecmw_restart_add_real(restart_time, restrt_data_size)
+      restrt_data_size = size(fstrHEAT%TEMP)
+      call hecmw_restart_add_real(fstrHEAT%TEMP, restrt_data_size)
+      call hecmw_restart_write()
+      if( hecMESH%my_rank.eq.0 ) then
+        write(IMSG,*) '### FSTR output Restart_File.'
+        call flush(IMSG)
+      endif
+    endif
+  end subroutine heat_output_restart
+end module m_heat_io
---
+++ fistr1/src/analysis/heat/heat_solve_main.f90
@@ -0,0 +1,88 @@
+!-------------------------------------------------------------------------------
+! Copyright (c) 2016 The University of Tokyo
+! This software is released under the MIT License, see LICENSE.txt
+!-------------------------------------------------------------------------------
+!> This module provides a function for stationary heat analysis
+module m_heat_solve_main
+contains
+
+  subroutine heat_solve_main(hecMESH, hecMAT, hecMATmpc, fstrPARAM, fstrHEAT, ISTEP, next_time, DTIME)
+    use m_fstr
+    use m_heat_mat_ass_conductivity
+    use m_heat_mat_ass_capacity
+    use m_heat_mat_ass_boundary
+    use m_solve_lineq
+    implicit none
+    integer(kind=kint) :: i, iterALL, ISTEP, bup_n_dof
+    real(kind=kreal)   :: DTIME, next_time
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(hecmwST_matrix)      :: hecMAT
+    type(fstr_heat)           :: fstrHEAT
+    type(fstr_param)          :: fstrPARAM
+    type(hecmwST_matrix), pointer :: hecMATmpc
+    logical :: is_congerged
+
+    iterALL = 0
+    do
+      iterALL = iterALL + 1
+      hecMAT%X = 0.0d0
+
+      !call heat_mat_ass_conductivity(hecMESH, hecMAT, fstrHEAT)
+      !if(fstrHEAT%is_steady == 0) call heat_mat_ass_capacity(hecMESH, hecMAT, fstrHEAT, DTIME)
+      !call heat_mat_ass_boundary(hecMESH, hecMAT, hecMATmpc, fstrHEAT, next_time, DTIME)
+
+      hecMATmpc%Iarray(97) = 1 !Need numerical factorization
+      bup_n_dof = hecMESH%n_dof
+      hecMESH%n_dof = 1
+      call solve_LINEQ(hecMESH, hecMATmpc)
+      hecMESH%n_dof = bup_n_dof
+      call hecmw_mpc_tback_sol(hecMESH, hecMAT, hecMATmpc)
+
+      do i = 1, hecMESH%n_node
+        fstrHEAT%TEMPC(i) = fstrHEAT%TEMP(i)
+        fstrHEAT%TEMP (i) = hecMAT%X(i)
+      enddo
+
+      call heat_check_convergence(hecMESH, fstrHEAT, fstrPARAM, ISTEP, iterALL, is_congerged)
+
+      if(is_congerged .or. fstrHEAT%is_iter_max_limit) exit
+    enddo
+  end subroutine heat_solve_main
+
+  subroutine heat_check_convergence(hecMESH, fstrHEAT, fstrPARAM, ISTEP, iterALL, is_congerged)
+    use m_fstr
+    implicit none
+    integer(kind=kint) :: i, iterALL, iter_max, ISTEP
+    real(kind=kreal)   :: val, iter_eps
+    type(hecmwST_local_mesh)  :: hecMESH
+    type(fstr_heat)           :: fstrHEAT
+    type(fstr_param)          :: fstrPARAM
+    logical :: is_congerged
+
+    is_congerged = .false.
+    fstrHEAT%is_iter_max_limit = .false.
+    iter_max = fstrPARAM%itmax(ISTEP)
+    iter_eps = fstrPARAM%eps(ISTEP)
+
+    val = 0.0d0
+    do i = 1, hecMESH%nn_internal
+      val = val + (fstrHEAT%TEMP(i) - fstrHEAT%TEMPC(i))**2
+    enddo
+    call hecmw_allREDUCE_R1(hecMESH, val, hecmw_sum)
+    val = dsqrt(val)
+
+    if(hecMESH%my_rank == 0)then
+      write(*,'(i8,1pe16.6)') iterALL, val
+    endif
+
+    if(val < iter_eps)then
+      if(hecMESH%my_rank == 0) write(IMSG,*) ' !!! CONVERGENCE ACHIEVED '
+      is_congerged = .true.
+    endif
+
+    if(iter_max <= iterALL)then
+      if(hecMESH%my_rank == 0) write(*,*) ' !!! ITERATION COUNT OVER : MAX = ', iter_max
+      fstrHEAT%is_iter_max_limit = .true.
+    endif
+  end subroutine heat_check_convergence
+end module m_heat_solve_main
