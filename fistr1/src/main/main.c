4a9651858a4017333a77babf3a60245e8fa29c64
Michio Ogawa <michioga@bekkoame.ne.jp>
2019-04-04 00:35:56 +0000

    Michioga/set openmp

--- fistr1/src/main/main.c
+++ fistr1/src/main/main.c
@@ -3,26 +3,70 @@
  * This software is released under the MIT License, see LICENSE.txt
  *****************************************************************************/
 /**
- * startup routine for FrontISTR
+ * \brief Startup routine for FrontISTR
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <FrontISTRConfig.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif /* _OPENMP */
 
 extern void fstr_main();
 
-void help() {
+/**
+ * \brief struct of command-line option
+ */
+struct option_rec {
+  char *option_name;
+  void (*func)(char *);
+};
+
+#ifdef _OPENMP
+/**
+ * \brief Set number of OpenMP threads
+ */
+void set_num_threads(char *arg) {
+  int exec_threads;
+
+  if (arg == NULL) {
+    fprintf(stderr, "Error : specify number of OpenMP threads.\n");
+    fprintf(stderr, "Format: -t <n>\n");
+    exit(1);
+  }
+
+  exec_threads = atoi(arg);
+
+  if (exec_threads == 0) {
+    fprintf(stderr, "Error : specify 1 or more OpenMP threads.\n");
+    exit(1);
+  }
+  omp_set_num_threads(exec_threads);
+}
+#endif /* _OPENMP */
+
+/**
+ * \brief show available command line option
+ */
+void help(char *arg) {
   printf("usage: [ mpirun -np <mpiprocs> ] fistr1 [options] \n");
-  printf("-h: Show this help message.\n");
-  printf(
-      "-c <Path of control file>: Use this control file. Default "
-      "./hecmw_ctrl.dat\n");
-  printf("-v: Show version.\n");
+  printf(" -h: Show this help message.\n");
+  printf(" -v: Show version.\n");
+#ifdef _OPENMP
+  printf(" -t <n>: Set number of OpenMP threads\n");
+#endif
+  printf(" -c <Path of control file>: Use this control file. Default "
+         "./hecmw_ctrl.dat\n");
   exit(0);
 }
 
-void version() {
+/**
+ * \brief show version and revision of FrontISTR
+ */
+void version(char *arg) {
   printf("FrontISTR version %d.%d.%d (%s) \n", VERSION_MAJOR, VERSION_MINOR,
          VERSION_PATCH, GIT_HASH);
 #ifdef WITH_MPI
@@ -74,27 +118,45 @@ void version() {
   exit(0);
 }
 
-int main(int argc, char *argv[]) {
-  int i;
+/**
+ * \brief load hecmw_ctrl.dat from specified place
+ */
+void load_hecmw_ctrl(char *arg) {
+  fprintf(stderr, "Sorry this option cannot work yet. (-c)\n");
+  fprintf(stderr, "%s\n", arg);
+  exit(0);
+}
 
-  for (i = 0; i < argc; ++i) {
-    if (*argv[i] == '-') {
-      switch (*(argv[i] + 1)) {
-        case 'h':
-        case 'H':
-          help();
-          break;
+/**
+ * \brief specify command line option name and executing function name.
+ * \attension list must be terminated with NULL value.
+ */
+struct option_rec options[] = {
+  {"-h", help},
+  {"-H", help},
+  {"-v", version},
+  {"-V", version},
+#ifdef _OPENMP
+  {"-t", set_num_threads},
+  {"-T", set_num_threads},
+#endif /* _OPENMP */
+  {"-c", load_hecmw_ctrl},
+  {"-C", load_hecmw_ctrl},
+  {NULL, NULL}
+};
 
-        case 'v':
-        case 'V':
-          version();
-          break;
+/**
+ * \brief main function
+ */
+int main(int argc, char *argv[])
+{
+  struct option_rec *p;
+  unsigned int i;
 
-        case 'c':
-          i++;
-          printf("Sorry this option cannot work yet. (%s)\n", argv[i]);
-          exit(0);
-          break;
+  for (i = 0; i < argc; i++) {
+    for (p = options; p->option_name != NULL; p++) {
+      if (strncmp(p->option_name, argv[i], strlen(p->option_name)) == 0) {
+        p->func(argv[i + 1]);
       }
     }
   }
