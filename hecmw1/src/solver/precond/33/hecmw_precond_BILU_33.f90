547d5c31444b3bf83e9a751fdfbd9d6e10180d69
Naoyuki Sogo <sogo@longtail-software.co.jp>
2022-11-26 17:09:19 +0900

    fix loop exchaged version (opt1)

--- hecmw1/src/solver/precond/33/hecmw_precond_BILU_33.f90
+++ hecmw1/src/solver/precond/33/hecmw_precond_BILU_33.f90
@@ -11,13 +11,20 @@
 module hecmw_precond_BILU_33
   use hecmw_util
   use hecmw_matrix_misc
+  use m_hecmw_matrix_ordering_CM
+  use m_hecmw_matrix_ordering_MC
+  use hecmw_matrix_reorder
+  use hecmw_matrix_contact
+  !$ use omp_lib
 
   private
+  integer(kind=kint), parameter :: DEBUG = 1
 
   public:: hecmw_precond_BILU_33_setup
   public:: hecmw_precond_BILU_33_apply
   public:: hecmw_precond_BILU_33_apply_opt1
   public:: hecmw_precond_BILU_33_apply_opt2
+  public:: hecmw_precond_BILU_33_apply_opt3
   public:: hecmw_precond_BILU_33_clear
 
   integer(kind=kint) :: N
@@ -28,7 +35,37 @@ module hecmw_precond_BILU_33
   integer(kind=kint), pointer :: inumFI1U(:) => null()
   integer(kind=kint), pointer :: FI1L(:) => null()
   integer(kind=kint), pointer :: FI1U(:) => null()
+  integer(kind=kint), pointer :: indexL(:) => null()
+  integer(kind=kint), pointer :: indexU(:) => null()
+  integer(kind=kint), pointer :: itemL(:) => null()
+  integer(kind=kint), pointer :: itemU(:) => null()
+  real(kind=kreal), pointer :: ALU(:) => null()
 
+  integer(kind=kint) :: NContact = 0
+  real(kind=kreal), pointer :: CAL(:) => null()
+  real(kind=kreal), pointer :: CAU(:) => null()
+  integer(kind=kint), pointer :: indexCL(:) => null()
+  integer(kind=kint), pointer :: indexCU(:) => null()
+  integer(kind=kint), pointer :: itemCL(:) => null()
+  integer(kind=kint), pointer :: itemCU(:) => null()
+
+  integer(kind=kint) :: NColor
+  integer(kind=kint), pointer :: COLORindex(:) => null()
+  integer(kind=kint), pointer :: perm(:) => null()
+  integer(kind=kint), pointer :: iperm(:) => null()
+
+  ! for tuning
+  integer(kind=kint), parameter :: numOfBlockPerThread = 100
+  integer(kind=kint), save :: numOfThread = 1, numOfBlock
+  integer(kind=kint), save, allocatable :: icToBlockIndex(:)
+  integer(kind=kint), save, allocatable :: blockIndexToColorIndex(:)
+
+  integer(kind=kint) :: jmaxU, jmaxL
+  real(kind=kreal), pointer :: SW1(:) => null()
+  real(kind=kreal), pointer :: SW2(:) => null()
+  real(kind=kreal), pointer :: SW3(:) => null()
+
+  logical, save :: isFirst = .true.
   logical, save :: INITIALIZED = .false.
 
 contains
@@ -36,17 +73,28 @@ contains
   subroutine hecmw_precond_BILU_33_setup(hecMAT)
     implicit none
     type(hecmwST_matrix), intent(inout) :: hecMAT
-    integer(kind=kint ) :: NP, NPU, NPL
+    integer(kind=kint ) :: NP, NPU, NPL, NPCL, NPCU
+    real   (kind=kreal), allocatable :: CD(:)
     integer(kind=kint ) :: PRECOND
     real   (kind=kreal) :: SIGMA, SIGMA_DIAG
 
     real(kind=kreal), pointer :: D(:)
     real(kind=kreal), pointer :: AL(:)
     real(kind=kreal), pointer :: AU(:)
+    integer(kind=kint ) :: ii, i, j, k
 
     integer(kind=kint ), pointer :: INL(:), INU(:)
     integer(kind=kint ), pointer :: IAL(:)
     integer(kind=kint ), pointer :: IAU(:)
+    integer(kind=kint ) :: NCOLOR_IN
+    integer(kind=kint ), allocatable :: perm_tmp(:)
+    real   (kind=kreal) :: t0
+    integer(kind=kint) :: isL, ieL, isU, ieU
+
+    if (DEBUG >= 1) then
+      t0 = hecmw_Wtime()
+      write(0,*) 'DEBUG: BILU setup start', hecmw_Wtime()-t0
+    endif
 
     if (INITIALIZED) then
       if (hecMAT%Iarray(98) == 1) then ! need symbolic and numerical setup
@@ -83,11 +131,161 @@ contains
       &   (N, NP, NPL, NPU, D, AL, INL, IAL, AU, INU, IAU, &
       &    SIGMA, SIGMA_DIAG)
 
+    NCOLOR_IN = hecmw_mat_get_ncolor_in(hecMAT)
+    NContact = hecMAT%cmat%n_val
+
+    if (NContact.gt.0) then
+      call hecmw_cmat_LU( hecMAT )
+    endif
+
+    allocate(COLORindex(0:N), perm_tmp(N), perm(N), iperm(N))
+    call hecmw_matrix_ordering_RCM(N, hecMAT%indexL, hecMAT%itemL, &
+      hecMAT%indexU, hecMAT%itemU, perm_tmp, iperm)
+    if (DEBUG >= 1) write(0,*) 'DEBUG: RCM ordering done', hecmw_Wtime()-t0
+    call hecmw_matrix_ordering_MC(N, hecMAT%indexL, hecMAT%itemL, &
+      hecMAT%indexU, hecMAT%itemU, perm_tmp, &
+      NCOLOR_IN, NColor, COLORindex, perm, iperm)
+    if (DEBUG >= 1) write(0,*) 'DEBUG: MC ordering done', hecmw_Wtime()-t0
+    deallocate(perm_tmp)
+    NPL = hecMAT%indexL(N)
+    NPU = hecMAT%indexU(N)
+    allocate(indexL(0:N), indexU(0:N), itemL(NPL), itemU(NPU))
+    call hecmw_matrix_reorder_profile(N, perm, iperm, &
+      hecMAT%indexL, hecMAT%indexU, hecMAT%itemL, hecMAT%itemU, &
+      indexL, indexU, itemL, itemU)
+    if (DEBUG >= 1) write(0,*) 'DEBUG: reordering profile done', hecmw_Wtime()-t0
+
+    !call check_ordering
+
+    allocate(D(9*N), AL(9*NPL), AU(9*NPU))
+    call hecmw_matrix_reorder_values(N, 3, perm, iperm, &
+      hecMAT%indexL, hecMAT%indexU, hecMAT%itemL, hecMAT%itemU, &
+      hecMAT%AL, hecMAT%AU, hecMAT%D, &
+      indexL, indexU, itemL, itemU, AL, AU, D)
+    if (DEBUG >= 1) write(0,*) 'DEBUG: reordering values done', hecmw_Wtime()-t0
+
+    call hecmw_matrix_reorder_renum_item(N, perm, indexL, itemL)
+    call hecmw_matrix_reorder_renum_item(N, perm, indexU, itemU)
+
+    if (NContact.gt.0) then
+      NPCL = hecMAT%indexCL(N)
+      NPCU = hecMAT%indexCU(N)
+      allocate(indexCL(0:N), indexCU(0:N), itemCL(NPCL), itemCU(NPCU))
+      call hecmw_matrix_reorder_profile(N, perm, iperm, &
+        hecMAT%indexCL, hecMAT%indexCU, hecMAT%itemCL, hecMAT%itemCU, &
+        indexCL, indexCU, itemCL, itemCU)
+
+      allocate(CD(9*N), CAL(9*NPCL), CAU(9*NPCU))
+      call hecmw_matrix_reorder_values(N, 3, perm, iperm, &
+        hecMAT%indexCL, hecMAT%indexCU, hecMAT%itemCL, hecMAT%itemCU, &
+        hecMAT%CAL, hecMAT%CAU, hecMAT%D, &
+        indexCL, indexCU, itemCL, itemCU, CAL, CAU, CD)
+      deallocate(CD)
+
+      call hecmw_matrix_reorder_renum_item(N, perm, indexCL, itemCL)
+      call hecmw_matrix_reorder_renum_item(N, perm, indexCU, itemCU)
+    end if
+
+    allocate(ALU(9*N))
+    ALU  = 0.d0
+
+    do ii= 1, 9*N
+      ALU(ii) = D(ii)
+    enddo
+
+    if (NContact.gt.0) then
+      do k= 1, hecMAT%cmat%n_val
+        if (hecMAT%cmat%pair(k)%i.ne.hecMAT%cmat%pair(k)%j) cycle
+        ii = iperm( hecMAT%cmat%pair(k)%i )
+        ALU(9*ii-8) = ALU(9*ii-8) + hecMAT%cmat%pair(k)%val(1, 1)
+        ALU(9*ii-7) = ALU(9*ii-7) + hecMAT%cmat%pair(k)%val(1, 2)
+        ALU(9*ii-6) = ALU(9*ii-6) + hecMAT%cmat%pair(k)%val(1, 3)
+        ALU(9*ii-5) = ALU(9*ii-5) + hecMAT%cmat%pair(k)%val(2, 1)
+        ALU(9*ii-4) = ALU(9*ii-4) + hecMAT%cmat%pair(k)%val(2, 2)
+        ALU(9*ii-3) = ALU(9*ii-3) + hecMAT%cmat%pair(k)%val(2, 3)
+        ALU(9*ii-2) = ALU(9*ii-2) + hecMAT%cmat%pair(k)%val(3, 1)
+        ALU(9*ii-1) = ALU(9*ii-1) + hecMAT%cmat%pair(k)%val(3, 2)
+        ALU(9*ii  ) = ALU(9*ii  ) + hecMAT%cmat%pair(k)%val(3, 3)
+      enddo
+    endif
+
+    jmaxU=0
+    jmaxL=0
+    do i=1, N
+      isL= inumFI1L(i-1)+1
+      ieL= inumFI1L(i)
+      if(jmaxL .lt. ieL-isL) then
+        jmaxL=ieL-isL+1
+      endif
+      isU= inumFI1U(i-1) + 1
+      ieU= inumFI1U(i)
+      if(jmaxU .lt. ieU-isU) then
+        jmaxU=ieU-isU+1
+      endif
+    enddo
+    allocate(SW1(N))
+    allocate(SW2(N))
+    allocate(SW3(N))
+
+
+    isFirst = .true.
     INITIALIZED = .true.
     hecMAT%Iarray(98) = 0 ! symbolic setup done
     hecMAT%Iarray(97) = 0 ! numerical setup done
 
+    if (DEBUG >= 1) write(0,*) "DEBUG: NCOLOR = ",NColor
+    if (DEBUG >= 1) write(0,*) "DEBUG: NContact = ",NContact
+    if (DEBUG >= 1) write(0,*) 'DEBUG: BILU setup done', hecmw_Wtime()-t0
+
   end subroutine hecmw_precond_BILU_33_setup
+
+  subroutine setup_tuning_parameters
+    implicit none
+    integer(kind=kint) :: blockIndex, elementCount, numOfElement, ii
+    real(kind=kreal) :: numOfElementPerBlock
+    integer(kind=kint) :: my_rank
+    integer(kind=kint) :: ic, i
+    if (DEBUG >= 1) write(*,*) 'DEBUG: setting up tuning parameters for BILU'
+#ifdef _OPENMP
+    numOfThread = omp_get_max_threads()
+#endif
+    numOfBlock = numOfThread * numOfBlockPerThread
+    if (allocated(icToBlockIndex)) deallocate(icToBlockIndex)
+    if (allocated(blockIndexToColorIndex)) deallocate(blockIndexToColorIndex)
+    allocate (icToBlockIndex(0:NColor), &
+         blockIndexToColorIndex(0:numOfBlock + NColor))
+    numOfElement = N + indexL(N) + indexU(N)
+    numOfElementPerBlock = dble(numOfElement) / numOfBlock
+    blockIndex = 0
+    icToBlockIndex = -1
+    icToBlockIndex(0) = 0
+    blockIndexToColorIndex = -1
+    blockIndexToColorIndex(0) = 0
+    my_rank = hecmw_comm_get_rank()
+    ! write(9000+my_rank,*) &
+    !      '# numOfElementPerBlock =', numOfElementPerBlock
+    ! write(9000+my_rank,*) &
+    !      '# ic, blockIndex, colorIndex, elementCount'
+    do ic = 1, NColor
+      elementCount = 0
+      ii = 1
+      do i = COLORindex(ic-1)+1, COLORindex(ic)
+        elementCount = elementCount + 1
+        elementCount = elementCount + (indexL(i) - indexL(i-1))
+        elementCount = elementCount + (indexU(i) - indexU(i-1))
+        if (elementCount > ii * numOfElementPerBlock &
+             .or. i == COLORindex(ic)) then
+          ii = ii + 1
+          blockIndex = blockIndex + 1
+          blockIndexToColorIndex(blockIndex) = i
+          ! write(9000+my_rank,*) ic, blockIndex, &
+          !      blockIndexToColorIndex(blockIndex), elementCount
+        endif
+      enddo
+      icToBlockIndex(ic) = blockIndex
+    enddo
+    numOfBlock = blockIndex
+  end subroutine setup_tuning_parameters
 
   subroutine hecmw_precond_BILU_33_apply(WW)
     implicit none
@@ -97,7 +295,6 @@ contains
     !C
     !C-- FORWARD
 
-!NEC$ ivdep
     do i= 1, N
       SW1= WW(3*i-2)
       SW2= WW(3*i-1)
@@ -130,7 +327,6 @@ contains
     !C
     !C-- BACKWARD
 
-!NEC$ ivdep
     do i= N, 1, -1
       isU= inumFI1U(i-1) + 1
       ieU= inumFI1U(i)
@@ -163,31 +359,20 @@ contains
   subroutine hecmw_precond_BILU_33_apply_opt1(WW)
     implicit none
     real(kind=kreal), intent(inout) :: WW(:)
-    integer(kind=kint) :: i, j, isL, ieL, isU, ieU, k, jmax,j_offset
+    integer(kind=kint) :: i, j, isL, ieL, isU, ieU, k
+    integer(kind=kint) :: j_offset
     real(kind=kreal) ::  X1, X2, X3
-    real(kind=kreal), allocatable :: SW1(:), SW2(:), SW3(:)
     !C
     !C-- FORWARD
 
-    jmax=0
-    do i=1, N
-      isL= inumFI1L(i-1)+1
-      ieL= inumFI1L(i)
-      if(jmax .lt. ieL - isL) then
-        jmax=ieL-isL+1
-      endif
+    do i= 1, N
+      SW1(i)= WW(3*i-2)
+      SW2(i)= WW(3*i-1)
+      SW3(i)= WW(3*i  )
     enddo
-    allocate(SW1(N))
-    allocate(SW2(N))
-    allocate(SW3(N))
-
-
-    do j_offset=1, jmax
+    do j_offset=0, jmaxL
 !NEC$ ivdep
       do i= 1, N
-        SW1(i)= WW(3*i-2)
-        SW2(i)= WW(3*i-1)
-        SW3(i)= WW(3*i  )
         isL= inumFI1L(i-1)+1
         ieL= inumFI1L(i)
         j=isL+j_offset
@@ -201,44 +386,35 @@ contains
           SW3(i)= SW3(i) - ALlu0(9*j-2)*X1-ALlu0(9*j-1)*X2-ALlu0(9*j  )*X3
         end if
       enddo
+      X1= SW1(i)
+      X2= SW2(i)
+      X3= SW3(i)
+      X2= X2 - Dlu0(9*i-5)*X1
+      X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
+      X3= Dlu0(9*i  )*  X3
+      X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
+      X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
+      WW(3*i-2)= X1
+      WW(3*i-1)= X2
+      WW(3*i  )= X3
     enddo
 
-!NEC$ ivdep
-      do i= 1, N
-        X1= SW1(i)
-        X2= SW2(i)
-        X3= SW3(i)
-        X2= X2 - Dlu0(9*i-5)*X1
-        X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
-        X3= Dlu0(9*i  )*  X3
-        X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
-        X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
-        WW(3*i-2)= X1
-        WW(3*i-1)= X2
-        WW(3*i  )= X3
-      enddo
 
     !C
     !C-- BACKWARD
-    jmax=0
-    do i=1, N
-        isU= inumFI1U(i-1) + 1
-        ieU= inumFI1U(i)
-      if(jmax .lt. ieU - isU) then
-        jmax=ieU-isU
-      endif
-    enddo
 
-    do j_offset=1, jmax
+    do i= N, 1, -1
+      SW1(i)= 0.d0
+      SW2(i)= 0.d0
+      SW3(i)= 0.d0
+    enddo
+    do j_offset=0, jmaxU
 !NEC$ ivdep
       do i= N, 1, -1
         isU= inumFI1U(i-1) + 1
         ieU= inumFI1U(i)
-        SW1(i)= 0.d0
-        SW2(i)= 0.d0
-        SW3(i)= 0.d0
         j=ieU-j_offset
-        if(j .ge. ieU) then
+        if(j .ge. isU) then
           k= FI1U(j)
           X1= WW(3*k-2)
           X2= WW(3*k-1)
@@ -248,23 +424,122 @@ contains
           SW3(i)= SW3(i) + AUlu0(9*j-2)*X1+AUlu0(9*j-1)*X2+AUlu0(9*j  )*X3
         end if
       enddo
+      X1= SW1(i)
+      X2= SW2(i)
+      X3= SW3(i)
+      X2= X2 - Dlu0(9*i-5)*X1
+      X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
+      X3= Dlu0(9*i  )*  X3
+      X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
+      X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
+      WW(3*i-2)=  WW(3*i-2) - X1
+      WW(3*i-1)=  WW(3*i-1) - X2
+      WW(3*i  )=  WW(3*i  ) - X3
     enddo
-      do i= N, 1, -1
-        X1= SW1(i)
-        X2= SW2(i)
-        X3= SW3(i)
-        X2= X2 - Dlu0(9*i-5)*X1
-        X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
-        X3= Dlu0(9*i  )*  X3
-        X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
-        X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
-        WW(3*i-2)=  WW(3*i-2) - X1
-        WW(3*i-1)=  WW(3*i-1) - X2
-        WW(3*i  )=  WW(3*i  ) - X3
-      enddo
   end subroutine hecmw_precond_BILU_33_apply_opt1
 
   subroutine hecmw_precond_BILU_33_apply_opt2(WW)
+    implicit none
+    real(kind=kreal), intent(inout) :: WW(:)
+    integer(kind=kint) :: i, j, isL, ieL, isU, ieU, k, j_offset
+    integer(kind=kint) :: ic
+    real(kind=kreal) ::  X1, X2, X3
+
+    integer(kind=kint) :: blockIndex
+    !C
+    !C-- FORWARD
+
+    if (isFirst) then
+      call setup_tuning_parameters
+      isFirst = .false.
+    endif
+
+    do i= 1, N
+      SW1(i)= WW(3*i-2)
+      SW2(i)= WW(3*i-1)
+      SW3(i)= WW(3*i  )
+    enddo
+    do j_offset=0, jmaxL
+      do ic=1,NColor
+        do blockIndex = icToBlockIndex(ic-1)+1, icToBlockIndex(ic)
+!NEC$ ivdep
+          do i = blockIndexToColorIndex(blockIndex-1)+1, &
+              blockIndexToColorIndex(blockIndex)
+          isL= inumFI1L(i-1)+1
+          ieL= inumFI1L(i)
+          j=isL+j_offset
+          if(j .le. ieL) then
+            k= FI1L(j)
+            X1= WW(3*k-2)
+            X2= WW(3*k-1)
+            X3= WW(3*k  )
+            SW1(i)= SW1(i) - ALlu0(9*j-8)*X1-ALlu0(9*j-7)*X2-ALlu0(9*j-6)*X3
+            SW2(i)= SW2(i) - ALlu0(9*j-5)*X1-ALlu0(9*j-4)*X2-ALlu0(9*j-3)*X3
+            SW3(i)= SW3(i) - ALlu0(9*j-2)*X1-ALlu0(9*j-1)*X2-ALlu0(9*j  )*X3
+          end if
+          X1= SW1(i)
+          X2= SW2(i)
+          X3= SW3(i)
+          X2= X2 - Dlu0(9*i-5)*X1
+          X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
+          X3= Dlu0(9*i  )*  X3
+          X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
+          X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
+          WW(3*i-2)= X1
+          WW(3*i-1)= X2
+          WW(3*i  )= X3
+        enddo
+      enddo
+    enddo
+    enddo
+
+    !C
+    !C-- BACKWARD
+
+    do i= N, 1, -1
+      SW1(i)= 0.d0
+      SW2(i)= 0.d0
+      SW3(i)= 0.d0
+    enddo
+    do j_offset=0, jmaxU
+      do ic=NColor, 1, -1
+        do blockIndex = icToBlockIndex(ic), icToBlockIndex(ic-1)+1, -1
+!NEC$ ivdep
+          do i = blockIndexToColorIndex(blockIndex), &
+              blockIndexToColorIndex(blockIndex-1)+1, -1
+            isU= inumFI1U(i-1) + 1
+            ieU= inumFI1U(i)
+            SW1(i)= 0.d0
+            SW2(i)= 0.d0
+            SW3(i)= 0.d0
+            j=ieU-j_offset
+            if(j .ge. isU) then
+              k= FI1U(j)
+              X1= WW(3*k-2)
+              X2= WW(3*k-1)
+              X3= WW(3*k  )
+              SW1(i)= SW1(i) + AUlu0(9*j-8)*X1+AUlu0(9*j-7)*X2+AUlu0(9*j-6)*X3
+              SW2(i)= SW2(i) + AUlu0(9*j-5)*X1+AUlu0(9*j-4)*X2+AUlu0(9*j-3)*X3
+              SW3(i)= SW3(i) + AUlu0(9*j-2)*X1+AUlu0(9*j-1)*X2+AUlu0(9*j  )*X3
+            end if
+          enddo
+          X1= SW1(i)
+          X2= SW2(i)
+          X3= SW3(i)
+          X2= X2 - Dlu0(9*i-5)*X1
+          X3= X3 - Dlu0(9*i-2)*X1 - Dlu0(9*i-1)*X2
+          X3= Dlu0(9*i  )*  X3
+          X2= Dlu0(9*i-4)*( X2 - Dlu0(9*i-3)*X3 )
+          X1= Dlu0(9*i-8)*( X1 - Dlu0(9*i-6)*X3 - Dlu0(9*i-7)*X2)
+          WW(3*i-2)=  WW(3*i-2) - X1
+          WW(3*i-1)=  WW(3*i-1) - X2
+          WW(3*i  )=  WW(3*i  ) - X3
+        enddo
+      enddo
+    enddo
+  end subroutine hecmw_precond_BILU_33_apply_opt2
+
+  subroutine hecmw_precond_BILU_33_apply_opt3(WW)
     implicit none
     real(kind=kreal), intent(inout) :: WW(:)
     integer(kind=kint) :: i, j, isL, ieL, isU, ieU, k
@@ -1487,7 +1762,7 @@ contains
       WW(3*i-1)=  WW(3*i-1) - X2
       WW(3*i  )=  WW(3*i  ) - X3
     enddo
-  end subroutine hecmw_precond_BILU_33_apply_opt2
+  end subroutine hecmw_precond_BILU_33_apply_opt3
 
   subroutine hecmw_precond_BILU_33_clear()
     implicit none
