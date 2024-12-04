44e60dd470188c71fa60a20426ced545a83bb6b6
inagaki <inagaki.kazuhisa@gmail.com>
2024-12-04 19:10:15 +0900

    create fstr_StiffMatrix_elem

--- fistr1/src/analysis/static/fstr_StiffMatrix.f90
+++ fistr1/src/analysis/static/fstr_StiffMatrix.f90
@@ -6,6 +6,7 @@
 
 module m_fstr_StiffMatrix
   use m_fstr
+  use m_static_LIB
   implicit none
 
   private
@@ -17,7 +18,6 @@ contains
   !> \brief This subroutine creates tangential stiffness matrix
   subroutine fstr_StiffMatrix( hecMESH, hecMAT, fstrSOLID, time, tincr)
     !---------------------------------------------------------------------*
-    use m_static_LIB
     use mMechGauss
 
     type (hecmwST_local_mesh)  :: hecMESH      !< mesh information
@@ -25,8 +25,6 @@ contains
     type (fstr_solid)          :: fstrSOLID    !< we need boundary conditions of curr step
     real(kind=kreal),intent(in) :: time        !< current time
     real(kind=kreal),intent(in) :: tincr       !< time increment
-
-    type( tMaterial ), pointer :: material     !< material information
 
     real(kind=kreal)   :: stiffness(fstrSOLID%max_ncon_stf*6, fstrSOLID%max_ncon_stf*6)
     integer(kind=kint) :: nodLOCAL(fstrSOLID%max_ncon)
@@ -54,7 +52,7 @@ contains
       ! ----- element loop
       !$omp parallel default(none), &
         !$omp&  private(icel,iiS,nn,j,nodLOCAL,i,ecoord,du,u,u_prev,tt,cdsys_ID,coords, &
-        !$omp&          material,thick,stiffness,isect,ihead), &
+        !$omp&          thick,stiffness,isect,ihead), &
         !$omp&  shared(iS,iE,hecMESH,ndof,fstrSOLID,ic_type,hecMAT,time,tincr)
       !$omp do
       do icel= is, iE
@@ -84,85 +82,12 @@ contains
         if( cdsys_ID > 0 ) call get_coordsys(cdsys_ID, hecMESH, fstrSOLID, coords)
         thick = hecMESH%section%sect_R_item(ihead+1)
         if( getSpaceDimension( ic_type )==2 ) thick =1.d0
-        material => fstrSOLID%elements(icel)%gausses(1)%pMaterial
 
-        if( ic_type==241 .or. ic_type==242 .or. ic_type==231 .or. ic_type==232 .or. ic_type==2322) then
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_C2( ic_type,nn,ecoord(1:2,1:nn),fstrSOLID%elements(icel)%gausses(:),thick,  &
-            stiffness(1:nn*ndof,1:nn*ndof), fstrSOLID%elements(icel)%iset,          &
-            u(1:2,1:nn) )
+        call fstr_StiffMatrix_elem( ic_type, nn, ndof, nodLOCAL, ecoord, u, du, u_prev, fstrSOLID%sections(isect), & 
+        & hecMESH%section%sect_R_item(ihead+1:), cdsys_ID, coords, stiffness, fstrSOLID%elements(icel), &
+        & time, tincr, tt, thick )
 
-        elseif ( ic_type==301 ) then
-          call STF_C1( ic_type,nn,ecoord(:,1:nn),thick,fstrSOLID%elements(icel)%gausses(:),   &
-            stiffness(1:nn*ndof,1:nn*ndof), u(1:3,1:nn) )
-
-        elseif ( ic_type==361 ) then
-          if( fstrSOLID%sections(isect)%elemopt361 == kel361FI ) then ! full integration element
-            call STF_C3                                                                              &
-              ( ic_type, nn, ecoord(:, 1:nn), fstrSOLID%elements(icel)%gausses(:),                &
-              stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
-          else if( fstrSOLID%sections(isect)%elemopt361 == kel361BBAR ) then ! B-bar element
-            call STF_C3D8Bbar                                                                        &
-              ( ic_type, nn, ecoord(:, 1:nn), fstrSOLID%elements(icel)%gausses(:),                &
-              stiffness(1:nn*ndof,1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3, 1:nn), tt(1:nn) )
-          else if( fstrSOLID%sections(isect)%elemopt361 == kel361IC ) then ! incompatible element
-            call STF_C3D8IC                                                              &
-              ( ic_type, nn, ecoord(:,1:nn), fstrSOLID%elements(icel)%gausses(:), &
-              stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), &
-              fstrSOLID%elements(icel)%aux, tt(1:nn) )
-          else if( fstrSOLID%sections(isect)%elemopt361 == kel361FBAR ) then ! F-bar element
-            call STF_C3D8Fbar                                                                        &
-              ( ic_type, nn, ecoord(:, 1:nn), fstrSOLID%elements(icel)%gausses(:),                &
-              stiffness(1:nn*ndof,1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3, 1:nn), tt(1:nn) )
-          endif
-
-        elseif (ic_type==341 .or. ic_type==351 .or. ic_type==342 .or. ic_type==352 .or. ic_type==362 ) then
-          if( ic_type==341 .and. fstrSOLID%sections(isect)%elemopt341 == kel341SESNS ) cycle ! skip smoothed fem
-          call STF_C3                                                                              &
-            ( ic_type, nn, ecoord(:, 1:nn), fstrSOLID%elements(icel)%gausses(:),                &
-            stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
-
-        else if( ic_type == 511) then
-          call STF_CONNECTOR( ic_type,nn,ecoord(:,1:nn),fstrSOLID%elements(icel)%gausses(:),   &
-            stiffness(1:nn*ndof,1:nn*ndof), u(1:3,1:nn), tincr, tt(1:nn) )
-
-        else if( ic_type == 611) then
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_Beam(ic_type, nn, ecoord, hecMESH%section%sect_R_item(ihead+1:), &
-            &   material%variables(M_YOUNGS), material%variables(M_POISSON), stiffness(1:nn*ndof,1:nn*ndof))
-
-        else if( ic_type == 641 ) then
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_Beam_641(ic_type, nn, ecoord, fstrSOLID%elements(icel)%gausses(:), &
-            &            hecMESH%section%sect_R_item(ihead+1:), stiffness(1:nn*ndof,1:nn*ndof))
-
-        else if( ( ic_type == 741 ) .or. ( ic_type == 743 ) .or. ( ic_type == 731 ) ) then
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_Shell_MITC(ic_type, nn, ndof, ecoord(1:3, 1:nn), fstrSOLID%elements(icel)%gausses(:), &
-            &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 0)
-
-        else if( ic_type == 761 ) then   !for shell-solid mixed analysis
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_Shell_MITC(731, 3, 6, ecoord(1:3, 1:3), fstrSOLID%elements(icel)%gausses(:), &
-            &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 2)
-
-        else if( ic_type == 781 ) then   !for shell-solid mixed analysis
-          if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
-          call STF_Shell_MITC(741, 4, 6, ecoord(1:3, 1:4), fstrSOLID%elements(icel)%gausses(:), &
-            &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 1)
-
-        elseif ( ic_type==3414 ) then
-          if( material%mtype /= INCOMP_NEWTONIAN) call StiffMat_abort( ic_type, 3, material%mtype )
-          call STF_C3_vp                                                           &
-            ( ic_type, nn, ecoord(:, 1:nn),fstrSOLID%elements(icel)%gausses(:), &
-            stiffness(1:nn*ndof, 1:nn*ndof), tincr, u_prev(1:4, 1:nn) )
-        else if ( ic_type == 881 .or. ic_type == 891 ) then  !for selective es/ns smoothed fem
-          call STF_C3D4_SESNS                                                                   &
-            ( ic_type,nn,nodLOCAL,ecoord(:, 1:nn), fstrSOLID%elements(icel)%gausses(:),         &
-            stiffness, cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
-        else
-          call StiffMat_abort( ic_type, 1 )
-        endif
+        if( ic_type==341 .and. fstrSOLID%sections(isect)%elemopt341 == kel341SESNS ) cycle ! skip smoothed fem
         !
         ! ----- CONSTRUCT the GLOBAL MATRIX STARTED
         call hecmw_mat_ass_elem(hecMAT, nn, nodLOCAL, stiffness)
@@ -173,6 +98,110 @@ contains
     enddo        ! itype
 
   end subroutine fstr_StiffMatrix
+
+  subroutine fstr_StiffMatrix_elem( ic_type, nn, ndof, nodLOCAL, ecoord, u, du, u_prev, section, sect_R_item, &
+    & cdsys_ID, coords, stiffness, element, time, tincr, tt, thick )
+    integer, intent(in)                :: ic_type      !< element type
+    integer, intent(inout)             :: nn
+    integer, intent(in)                :: ndof
+    integer(kind=kint), intent(inout)  :: nodLOCAL(:)
+    real(kind=kreal),intent(in)        :: ecoord(:,:) 
+    real(kind=kreal),intent(in)        :: u(:,:) 
+    real(kind=kreal),intent(in)        :: du(:,:) 
+    real(kind=kreal),intent(in)        :: u_prev(:,:)
+    type(tSection),intent(in)          :: section
+    real(kind=kreal), intent(in)       :: sect_R_item(:)      
+    integer, intent(in)                :: cdsys_ID     
+    real(kind=kreal), intent(inout)    :: coords(3,3) 
+    real(kind=kreal), intent(out)      :: stiffness(:,:) 
+    type(tElement), intent(inout)      :: element    
+    real(kind=kreal),intent(in)        :: time      !< current time
+    real(kind=kreal),intent(in)        :: tincr     !< time increment
+    real(kind=kreal),intent(in)        :: tt(:) 
+    real(kind=kreal),intent(in)        :: thick
+
+    type( tMaterial ), pointer :: material     !< material information
+
+    material => element%gausses(1)%pMaterial
+
+    if( ic_type==241 .or. ic_type==242 .or. ic_type==231 .or. ic_type==232 .or. ic_type==2322) then
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_C2( ic_type,nn,ecoord(1:2,1:nn),element%gausses(:),thick,  &
+        stiffness(1:nn*ndof,1:nn*ndof), element%iset,          &
+        u(1:2,1:nn) )
+
+    elseif ( ic_type==301 ) then
+      call STF_C1( ic_type,nn,ecoord(:,1:nn),thick,element%gausses(:),   &
+        stiffness(1:nn*ndof,1:nn*ndof), u(1:3,1:nn) )
+
+    elseif ( ic_type==361 ) then
+      if( section%elemopt361 == kel361FI ) then ! full integration element
+        call STF_C3                                                                              &
+          ( ic_type, nn, ecoord(:, 1:nn), element%gausses(:),                &
+          stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
+      else if( section%elemopt361 == kel361BBAR ) then ! B-bar element
+        call STF_C3D8Bbar                                                                        &
+          ( ic_type, nn, ecoord(:, 1:nn), element%gausses(:),                &
+          stiffness(1:nn*ndof,1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3, 1:nn), tt(1:nn) )
+      else if( section%elemopt361 == kel361IC ) then ! incompatible element
+        call STF_C3D8IC                                                              &
+          ( ic_type, nn, ecoord(:,1:nn), element%gausses(:), &
+          stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), &
+          element%aux, tt(1:nn) )
+      else if( section%elemopt361 == kel361FBAR ) then ! F-bar element
+        call STF_C3D8Fbar                                                                        &
+          ( ic_type, nn, ecoord(:, 1:nn), element%gausses(:),                &
+          stiffness(1:nn*ndof,1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3, 1:nn), tt(1:nn) )
+      endif
+
+    elseif (ic_type==341 .or. ic_type==351 .or. ic_type==342 .or. ic_type==352 .or. ic_type==362 ) then
+      if( ic_type==341 .and. section%elemopt341 == kel341SESNS ) return ! skip smoothed fem
+      call STF_C3                                                                              &
+        ( ic_type, nn, ecoord(:, 1:nn), element%gausses(:),                &
+        stiffness(1:nn*ndof, 1:nn*ndof), cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
+
+    else if( ic_type == 511) then
+      call STF_CONNECTOR( ic_type,nn,ecoord(:,1:nn),element%gausses(:),   &
+        stiffness(1:nn*ndof,1:nn*ndof), u(1:3,1:nn), tincr, tt(1:nn) )
+
+    else if( ic_type == 611) then
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_Beam(ic_type, nn, ecoord, sect_R_item, &
+        &   material%variables(M_YOUNGS), material%variables(M_POISSON), stiffness(1:nn*ndof,1:nn*ndof))
+
+    else if( ic_type == 641 ) then
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_Beam_641(ic_type, nn, ecoord, element%gausses(:), &
+        &            sect_R_item, stiffness(1:nn*ndof,1:nn*ndof))
+
+    else if( ( ic_type == 741 ) .or. ( ic_type == 743 ) .or. ( ic_type == 731 ) ) then
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_Shell_MITC(ic_type, nn, ndof, ecoord(1:3, 1:nn), element%gausses(:), &
+        &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 0)
+
+    else if( ic_type == 761 ) then   !for shell-solid mixed analysis
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_Shell_MITC(731, 3, 6, ecoord(1:3, 1:3), element%gausses(:), &
+        &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 2)
+
+    else if( ic_type == 781 ) then   !for shell-solid mixed analysis
+      if( material%nlgeom_flag /= INFINITESIMAL ) call StiffMat_abort( ic_type, 2 )
+      call STF_Shell_MITC(741, 4, 6, ecoord(1:3, 1:4), element%gausses(:), &
+        &              stiffness(1:nn*ndof, 1:nn*ndof), thick, 1)
+
+    elseif ( ic_type==3414 ) then
+      if( material%mtype /= INCOMP_NEWTONIAN) call StiffMat_abort( ic_type, 3, material%mtype )
+      call STF_C3_vp                                                           &
+        ( ic_type, nn, ecoord(:, 1:nn),element%gausses(:), &
+        stiffness(1:nn*ndof, 1:nn*ndof), tincr, u_prev(1:4, 1:nn) )
+    else if ( ic_type == 881 .or. ic_type == 891 ) then  !for selective es/ns smoothed fem
+      call STF_C3D4_SESNS                                                                   &
+        ( ic_type,nn,nodLOCAL,ecoord(:, 1:nn), element%gausses(:),         &
+        stiffness, cdsys_ID, coords, time, tincr, u(1:3,1:nn), tt(1:nn) )
+    else
+      call StiffMat_abort( ic_type, 1 )
+    endif    
+  end subroutine
 
   subroutine StiffMat_abort( ic_type, flag, mtype )
     integer(kind=kint), intent(in)           :: ic_type
