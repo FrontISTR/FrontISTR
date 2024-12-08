c6a551ab84e3c434d3c64ec4d6fef0c87f14516a
Naoki MORITA <nmorita@kz.tsukuba.ac.jp>
2024-12-08 13:14:25 +0900

    Fix for OpenMP

--- fistr1/src/analysis/static/fstr_CreateMatrix.f90
+++ fistr1/src/analysis/static/fstr_CreateMatrix.f90
@@ -64,7 +64,7 @@ contains
 
       ! ----- element loop
       !$omp parallel default(none), &
-        !$omp&  private(icel,iiS,j,nodLOCAL,i,ecoord,du,u,u_prev,tt,cdsys_ID,coords, &
+        !$omp&  private(icel,iiS,j,nodLOCAL,i,ecoord,du,u,tt,cdsys_ID,coords, &
         !$omp&          material,thick,stiff_mat,isect,ihead,rho, length, surf, mass_mat, &
         !$omp&          lumped, sec_opt,a1,a2,a3,b1,b2,b3,c1,c2,damp_mat,mat,vecA,vecB,vecC,Kb,df,in,jn), &
         !$omp&  shared(iS,iE,hecMESH,nn,ndof,fstrSOLID,ic_type,hecMAT,time,tincr,fstrDYNAMIC, &
