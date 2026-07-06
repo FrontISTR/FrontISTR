a52876e4a6fd2bf74f24efdf0aff5f2c24a631a7
inagaki <inagaki.kazuhisa@gmail.com>
2026-07-06 01:58:58 +0000

    コード内Abaqus表現の除去（TYPE=ABAQUS → TYPE=INP, #757）

--- hecmw1/src/common/hecmw_conn_conv.h
+++ hecmw1/src/common/hecmw_conn_conv.h
@@ -7,7 +7,7 @@
 #define HECMW_CONN_CONV_INCLUDED
 
 #define HECMW_CONNTYPE_HECMW 1
-#define HECMW_CONNTYPE_ABAQUS 2
+#define HECMW_CONNTYPE_INP 2
 #define HECMW_CONNTYPE_NASTRAN 3
 
 extern int HECMW_convert_connectivity(int from, int hecmw_etype, int *conn);
