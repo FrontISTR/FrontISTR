eba9d1a7e053ad7e64ca4c44686a70b74ca0a9d9
Kazuya Goto <goto@pexprocs.jp>
2026-08-11 16:21:06 +0900

    Fix previous commit

--- fistr1/src/analysis/dynamic/transit/dynamic_mat_ass_bc_vl.f90
+++ fistr1/src/analysis/dynamic/transit/dynamic_mat_ass_bc_vl.f90
@@ -35,8 +35,7 @@ contains
       endif
 
       RHS = fstrSOLID%VELOCITY_ngrp_val(ig0)
-      call fstr_get_amplitude_dyn(hecMESH, fstrSOLID, fstrDYNAMIC, ig0, t_curr, f_t, flag_u)
-      RHS = RHS * f_t
+      call fstr_get_amplitude_dyn(hecMESH, fstrSOLID, fstrDYNAMIC, ig0, t_curr, RHS, flag_u)
 
       rid = fstrSOLID%VELOCITY_ngrp_rotID(ig0)
       ig  = fstrSOLID%VELOCITY_ngrp_ID(ig0)
