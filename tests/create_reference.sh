07e59ebbf78487072684ec2e52f3d97ef066113e
inagaki <inagaki.kazuhisa@gmail.com>
2026-09-02 19:25:43 +0900

    Support eigen analysis prior to frequency response analysis in tests
    - run the eigen analysis of <mesh>_eigen.cnt before the main analysis, and hand
      over its log file as eigen_0.log and its result files as <mesh>_eigen.res
    
    - register fstrDYNA so that the time history result <mesh>_dyna.res is written,
      and compare it with the reference as well
    
    - take the result file to merge and compare as an argument of check_result, so
      that step numbers of other result files in the working directory are not picked up

--- tests/create_reference.sh
+++ tests/create_reference.sh
@@ -38,11 +38,38 @@ done
 
 check_executable $fistr1
 
+write_hecmw_ctrl () {
+  ctrl_cnt=$1
+  ctrl_res=$2
+cat <<EOL > hecmw_ctrl.dat
+!MESH, NAME=fstrMSH,TYPE=HECMW-ENTIRE
+${mesh}
+!CONTROL,NAME=fstrCNT
+${ctrl_cnt}
+!RESULT,NAME=fstrRES,IO=OUT
+${ctrl_res}
+!RESULT,NAME=vis_out,IO=OUT
+${mesh}
+
+EOL
+  if [ -e $cnt_eig ]; then
+cat <<EOL >> hecmw_ctrl.dat
+!RESULT,NAME=fstrDYNA,IO=OUT
+${dres}
+!RESULT,NAME=result-in,IO=IN
+${eres}
+EOL
+  fi
+}
+
 for path in $(find . -not -path "./_archive/*" -type f -name "*.msh" ); do
   dir=$(dirname $path)
   mesh=$(basename $path)
   cnt=${mesh%.msh}.cnt
+  cnt_eig=${mesh%.msh}_eigen.cnt
   res=${mesh%.msh}.res
+  eres=${mesh%.msh}_eigen.res
+  dres=${mesh%.msh}_dyna.res
 
   if ls $dir/${res}.* > /dev/null 2>&1; then
     echo_err ${res}" exists. Skip this mesh file."
@@ -51,6 +78,7 @@ for path in $(find . -not -path "./_archive/*" -type f -name "*.msh" ); do
   # Remove previous results if exists
   rm -fr $dir/${mesh}_psf*
   rm -fr $dir/${res}*
+  rm -fr $dir/${dres}*
 
   if [ ! -e $dir/$cnt ]; then
     echo_err "*.cnt file for $path is not found. Skip this mesh file."
@@ -58,18 +86,18 @@ for path in $(find . -not -path "./_archive/*" -type f -name "*.msh" ); do
   fi
 
   pushd $dir
-cat <<EOL > hecmw_ctrl.dat
-!MESH, NAME=fstrMSH,TYPE=HECMW-ENTIRE
-${mesh}
-!CONTROL,NAME=fstrCNT
-${cnt}
-!RESULT,NAME=fstrRES,IO=OUT
-${res}
-!RESULT,NAME=vis_out,IO=OUT
-${mesh}
-
-EOL
+  #
+  # Frequency response analysis reads the modes of a preceding eigen analysis,
+  # so run it first and hand over its log file and result files
+  #
+  if [ -e $cnt_eig ]; then
+    write_hecmw_ctrl $cnt_eig $eres
+    $fistr1 -t 1
+    mv 0.log eigen_0.log
+  fi
+  write_hecmw_ctrl $cnt $res
   $fistr1 -t 1
-  rm -f hecmw_ctrl.dat FSTR.msg 0.log FSTR.sta FSTR.dbg.0 hecmw_vis.ini dyna*.txt
+  rm -f hecmw_ctrl.dat FSTR.msg 0.log eigen_0.log FSTR.sta FSTR.dbg.0 hecmw_vis.ini dyna*.txt
+  rm -f ${eres}.*
   popd
 done
